60a82288eb8a7d2eb54389c468bad94a6a41c724
Kevin Traver <kevintraver@gmail.com>
2013-06-14 23:02:13 -0700

    initial rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaptureWeb::Application.config.secret_token = '120bdca4e58581263a7805418f021f0eb384f5c662e107362f515d4819b4f0402d1394407425d11152c21723fa9f428e6ff09c3f13d6a4bd5b255d8826ce3b95'
