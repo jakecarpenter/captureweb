58197108fb82e7478a941e9a47e5cd98d34d8806
Kevin Traver <kevintraver@gmail.com>
2013-06-16 20:36:39 -0700

    remove avatar for now:

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,6 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.lastname = auth.info.last_name
       user.firstname = auth.info.first_name
-      user.avatar_from_url = auth.info.image
     end
   end
   
