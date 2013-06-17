a8e3e421adea7b66d089f48fb71c19e2d967537f
Kevin Traver <kevintraver@gmail.com>
2013-06-16 20:27:30 -0700

    fixed alias method in omniauth callback controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,5 +10,5 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
   alias_method :twitter, :all
-  alias_method :facebook, :all ?
+  alias_method :facebook, :all
 end
